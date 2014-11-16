9793757291da01239232c558642e6dae6cdcbd8d
Sergey Fedotov <sergey89@gmail.com>
2014-11-16 16:26:09 +0300

    Simple server script implementation

---
+++ server/upload.php
@@ -0,0 +1,43 @@
+<?php
+define('SECRET_KEY',    'secretword');
+define('UPLOAD_DIR',    __DIR__ . '/images');
+define('IMAGE_URL',     'http://example.com/images/%s');
+
+$secretKey = (string)filter_input(INPUT_GET, 'secretKey');
+
+if ($secretKey !== SECRET_KEY) {
+    http_response_code(403);
+    exit;
+}
+
+if (false === ($input = @fopen('php://input', 'rb'))) {
+    http_response_code(500);
+    exit;
+}
+
+$baseName = uniqid();
+$dirName = UPLOAD_DIR . '/' . substr($baseName, 0, 3);
+
+if (!is_dir($dirName)) {
+    if (!mkdir($dirName, 0755)) {
+        http_response_code(500);
+        exit;
+    }
+}
+
+$fileName = $baseName . '.png';
+$filePath = $dirName . '/' . substr($fileName, 3);
+
+if (false === ($output = @fopen($filePath, 'wb'))) {
+    http_response_code(500);
+    exit;
+}
+
+if (false === stream_copy_to_stream($input, $output)) {
+    http_response_code(500);
+    exit;
+}
+
+fclose($output);
+
+printf(IMAGE_URL, $fileName);
